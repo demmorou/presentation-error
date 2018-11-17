c0456a233c15c00e5b4801264665f986c4652486
Deusimar <sousadeusimar2@gmail.com>
2018-11-17 10:05:47 -0300

    comentario dos graficos

--- create_graph.py
+++ create_graph.py
@@ -3,13 +3,17 @@ from matplotlib.legend_handler import HandlerLine2D
 
 
 n = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+
+# 10 tests da bst - pior caso
 t_n_bst = [537.37, 1018.88, 1395.45, 2238.63, 2594.51, 3038.43, 3680.26, 4109.18, 4670.38, 5011.3]
+# 10 tests da avl - pior caso
 t_n_avl = [7.82, 8.94, 9.6, 10.1, 10.36, 10.64, 10.82, 10.92, 11.22, 11.44]
-# t_n_rbt = [1.3, 2.4, 3.5, 4.5, 5.8]
+# 10 tests da rbt - pior caso
+t_n_rbt = []
 
 line1, = plt.plot(n, t_n_bst, 'go', label='bst', linestyle='--')
 line2, = plt.plot(n, t_n_avl, 'bo', label='avl', linestyle='--')
-# line3, = plt.plot(n, t_n_rbt, 'ro', label='rbt', linestyle='--')
+line3, = plt.plot(n, t_n_rbt, 'ro', label='rbt', linestyle='--')
 
 plt.legend(handler_map={line1: HandlerLine2D(numpoints=4)})
 
